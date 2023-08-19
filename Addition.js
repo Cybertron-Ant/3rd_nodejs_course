9ba2d56accadbd7aa437eb7037e333a394032013
mediainformationofficer@gmail.com <mediainformationofficer@gmail.com>
2023-08-19 12:40:00 -0500

    We are then defining a function called 'AddNumber'. This function is defined to take 2 parameters, a and b. The function is added to the module "exports" to make the function as a public function that can be accessed by other application modules.

--- Addition.js
+++ Addition.js
@@ -1,2 +1,8 @@
 /*The "exports" keyword is used to ensure that the functionality defined in this file can actually be accessed by other files.*/
 exports = module.exports = {};
+
+
+/*We are then defining a function called 'AddNumber'. This function is defined to take 2 parameters, a and b. The function is added to the module "exports" to make the function as a public function that can be accessed by other application modules.*/
+exports.AddNumber = function(a, b) {
+  
+}
