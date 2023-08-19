812304d9026f8495678caca456edfb045c3a3381
mediainformationofficer@gmail.com <mediainformationofficer@gmail.com>
2023-08-19 12:49:51 -0500

    When you run the app.js file, you will get an output of value 3 in the console log. The result is because the AddNumber function in the Addition.js file was called successfully and the returned value of 3 was displayed in the console

--- runAddition.js
+++ runAddition.js
@@ -4,3 +4,6 @@ var Addition = require('./Addition.js');
 
 /*Since the functions in the Addition.js file are now accessible, we can now make a call to the AddNumber function. In the function, we are passing 2 numbers as parameters. We are then displaying the value in the console.*/
 console.log(Addition.AddNumber(1, 2));
+
+
+/*When you run the app.js file, you will get an output of value 3 in the console log. The result is because the AddNumber function in the Addition.js file was called successfully and the returned value of 3 was displayed in the console*/
