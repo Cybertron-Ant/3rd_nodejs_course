b512c9cd653cdd6f92d22c8751172ee658cdf808
mediainformationofficer@gmail.com <mediainformationofficer@gmail.com>
2023-08-19 13:35:48 -0500

    Since we have extended our Tutorial.js module and exposed a function called pTutor. It also calls the tutorial module in the Tutorial.js module

---
+++ runNodeTutorial.js
@@ -0,0 +1,5 @@
+
+var localTutor = require('./NodeTutorial.js');
+
+/*Since we have extended our Tutorial.js module and exposed a function called pTutor. It also calls the tutorial module in the Tutorial.js module*/
+localTutor.NodeTutorial();
